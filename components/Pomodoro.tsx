dcb2ffa8f1fa73df5e17be3e4db6485fd9c5fa3f
Nayel Abed Razi <mugwurp@gmail.com>
2025-04-28 06:44:21 +0530

    added pomodoro section...

--- components/Pomodoro.tsx
+++ components/Pomodoro.tsx
@@ -1,26 +1,39 @@
 import React from 'react'
 import { Orbitron } from 'next/font/google'
+import { Button } from './ui/button';
 
 const orbitron = Orbitron({subsets: ['latin'] });
 
 function Pomodoro() {
   return (
     <div className='flex m-5 mx-auto w-3/4 gap-3'> {/* Pomodoro Space*/}
+
       <div className='p-3 border-2 rounded-md w-2/3'> {/* Pomodoro Timer*/}
         <div className='flex items-start'>
             <h1 className='text-gray-800'>Pomodoro</h1>
         </div>
-        <div className='flex justify-center'>
+        <div className='flex justify-center m-8'>
             <h1 className={`${orbitron.className} text-8xl text-gray-800 font-orbitron font-bold`}>
-              00:00
+              25:00
             </h1>
         </div>
-
-
+        <div className='flex justify-center items-center gap-2 m-3'>
+          <Button>Start</Button>
+          <Button>Reset</Button>
+        </div>
       </div>
+
       <div className='p-3 border-2 rounded-md w-1/3'> {/* Pomodoro Counter*/}
-        <h1 className='text-gray-800'>Pomodoro Counter here</h1>
+        <div className='flex items-start'>
+            <h1 className='text-gray-800'>Pomodoro Counter here</h1>
+        </div>
+        <div className='flex justify-center m-8'>
+            <h1 className={`${orbitron.className} text-8xl text-gray-800 font-orbitron font-bold`}>
+              00
+            </h1>
+        </div>
       </div>
+
     </div>
   )
 }
